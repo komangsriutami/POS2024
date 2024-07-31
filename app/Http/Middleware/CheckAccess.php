e7d0bee158d45c6d3303cb301508f0727c283566
Komang Sri Utami <sriutami821@gmail.com>
2024-07-31 17:25:37 +0800

    --

--- app/Http/Middleware/CheckAccess.php
+++ app/Http/Middleware/CheckAccess.php
@@ -16,8 +16,11 @@ use App\MasterApotek;
 use App\MasterGroupApotek;
 use App\MasterTahun;
 use Cache;
+use Route;
+use Session;
+use Cache;
+use App\Traits\DynamicConnectionTrait;
 use DB;
-
 use App\Traits\DynamicConnectionTrait;
 
 class CheckAccess
